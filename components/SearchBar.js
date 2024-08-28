b84feecd3d15d1cb2d3da23f508038d421e10f4a
FW-Titi <136483262+FW-Titi@users.noreply.github.com>
2024-08-28 09:31:11 -0400

    Added build fixes

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { Input } from "@/components/ui/Input";
-import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function SearchBar({ onSearch }) {
   const [description, setDescription] = useState("");
